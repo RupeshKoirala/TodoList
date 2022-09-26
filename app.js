f18f5218adeecf700b55b85c88eee50384076d90
RupeshKoirala <rxkst244@mail.rmu.edu>
2022-09-26 12:32:43 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const items = ["Go shopping", "Go picnic", "Clean room"];
+
+
+app.get("/", function(req, res) {
+  res.render("list", {listTitle: "Today", newListItems: items});
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
+
+
+
+//const workItems = [];
+
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+// app.post("/", function(req, res){
+
+//   const item = req.body.newItem;
+
+//   if (req.body.list === "Work") {
+//     workItems.push(item);
+//     res.redirect("/work");
+//   } else {
+//     items.push(item);
+//     res.redirect("/");
+//   }
+// });
